c2eff167ceaa0dc8f3cd44e00368f77c40c1656b
mayacalabria <maya.calabria@students.olin.edu>
2018-03-30 15:17:20 -0400

    Create scaffolding

---
+++ NBAcode.py
@@ -0,0 +1,50 @@
+NBA code
+
+"""
+Project: NBA shot data visualization
+Authors: Bryce Mann, Juan Carlos del Rio, Maya Calabria
+Abstract: This project will pull data directly from https://stats.nba.com/ to offer
+            users a interactive map to visually explore the history of shot trends by
+            team, player, or conference.
+"""
+
+# Call data
+def data_construction():
+    """ Calls data from NBA as a JSON file and stores as a CSV """
+    pass
+
+def update_data():
+    """ Update data CSV with only new data, ignores old data """
+    pass
+
+# Parse court SVG
+def find_zones():
+    """ Find the lines on SVG file and name them accordingly """
+    pass
+
+# Sorting data
+def find_location():
+    pass
+
+def avg_shot():
+    """ Averages data based on what category is passed
+    i.e. team, specific player """
+    pass
+
+# Plotting data
+def build_heat_map():
+    pass
+
+def plot_heat_map():
+    """ Plots and saves file """
+    pass
+
+# UI wrapper
+### pygame?
+### drop down data set selection menu (year, division team, player, etc)
+### time scroll
+
+# Web app wrapper
+### prepare py files
+### upload to server
+### scaling?
