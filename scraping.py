abd3f4c681867d6e18cf5cb0439a1aae5a21f9d8
mayacalabria <maya.calabria@students.olin.edu>
2018-04-10 21:53:53 -0400

    Create scraping file

---
+++ scraping.py
@@ -0,0 +1,71 @@
+import pandas as pd
+# import nba_data as nba
+import numpy as np
+import requests as r
+import json
+
+url = 'http://stats.nba.com/stats/playerdashptshotlog?'+ \
+    'DateFrom=&DateTo=&GameSegment=&LastNGames=0&LeagueID=00&' + \
+    'Location=&Month=0&OpponentTeamID=0&Outcome=&Period=0&' + \
+    'PlayerID=201939&Season=2014-15&SeasonSegment=&' + \
+    'SeasonType=Regular+Season&TeamID=0&VsConference=&VsDivision='
+
+response = r.get(url)
+shots = response.json()['resultSets'][0]['rowSet']
+
+
+
+# # shots_url = 'https://stats.nba.com/stats/shotchartdetail?AheadBehind=&CFID=33&CFPARAMS=2017-18&ClutchTime=&Conference=&ContextFilter=&ContextMeasure=FGA&DateFrom=&DateTo=&Division=&EndPeriod=10&EndRange=28800&GROUP_ID=&GameEventID=&GameID=&GameSegment=&GroupID=&GroupMode=&GroupQuantity=5&LastNGames=0&LeagueID=00&Location=&Month=0&OnOff=&OpponentTeamID=0&Outcome=&PORound=0&Period=0&PlayerID=202330&PlayerID1=&PlayerID2=&PlayerID3=&PlayerID4=&PlayerID5=&PlayerPosition=&PointDiff=&Position=&RangeType=0&RookieYear=&Season=2017-18&SeasonSegment=&SeasonType=Regular+Season&ShotClockRange=&StartPeriod=1&StartRange=0&StarterBench=&TeamID=0&VsConference=&VsDivision=&VsPlayerID1=&VsPlayerID2=&VsPlayerID3=&VsPlayerID4=&VsPlayerID5=&VsTeamID=.json'
+# # # player_list = nba.player.commonallplayers(currentseason=0)
+# headers = {
+# 'User-Agent':'Mozilla/5.0 (platform; rv:geckoversion) Gecko/geckotrail Firefox/firefoxversion'
+# }
+# player_id = 201939
+# season = '2015-16'
+# season_type = 'Regular Season'
+#
+# # request parameters
+# req_params = {
+#  'AheadBehind': '',
+#  'ClutchTime': '',
+#  'ContextFilter': '',
+#  'ContextMeasure': 'FGA',
+#  'DateFrom': '',
+#  'DateTo': '',
+#  'EndPeriod': '',
+#  'EndRange': '',
+#  'GameID': '',
+#  'GameSegment': '',
+#  'LastNGames': 0,
+#  'LeagueID': '00',
+#  'Location': '',
+#  'Month': 0,
+#  'OpponentTeamID': 0,
+#  'Outcome': '',
+#  'Period': 0,
+#  'PlayerID': player_id,
+#  'PointDiff': '',
+#  'Position': '',
+#  'RangeType': '',
+#  'RookieYear': '',
+#  'Season': season,
+#  'SeasonSegment': '',
+#  'SeasonType': season_type,
+#  'StartPeriod': '',
+#  'StartRange': '',
+#  'TeamID': 0,
+#  'VsConference': '',
+#  'VsDivision': ''
+# }
+#
+# res = r.get('http://stats.nba.com/stats/shotchartdetail', params=req_params, headers=headers)
+#
+#
+# # response.raise_for_status()
+# shots = res.json()
+# rows = shots['resultSets'][0]['headers']
+# shots = shots['resultSets'][0]['rowSet']
+# df = pd.DataFrame(shots,columns=rows)
+#
+#
+#
