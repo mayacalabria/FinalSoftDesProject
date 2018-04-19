2eaff2082fa1c06a6b960c645f8420cd889298d6
mayacalabria <maya.calabria@students.olin.edu>
2018-04-19 18:27:24 -0400

    Add AR2 Framing

--- map_test.py
+++ map_test.py
@@ -2,9 +2,12 @@ import numpy as np
 from nba_py.player import get_player
 from nba_py import shotchart
 import matplotlib.pyplot as plt
+import matplotlib.lines as lines
 import plotly.plotly as py
 from scipy.stats import kendalltau
 import seaborn as sns
+from PIL import Image
+
 
 def generate_shots(first,last,season):
     """ Takes first, last, and season as string,
@@ -48,26 +51,32 @@ def success_heat_map(shots,display=True):
 
 def full_heat_map(shots,display=True):
     """ Creates heat map of all attempted shots from a pandas structure """
-    sns.set_style("white")
+    sns.set_style(None)
     xs = shots["LOC_X"]
     ys = shots["LOC_Y"]
     #heat map of all shots
-    sns.jointplot(xs, ys, kind="kde", stat_func=None, color="#bb38c8") ##4CB391
+    sns.jointplot(xs, ys, kind="kde", stat_func=None, color="#bb38c8", zorder=1) ##4CB391
     if display == True:
         title = '{} {}'.format(shots["PLAYER_NAME"][0], 'attempted shots')
         plt.title(title, x=-4,y=1.2)
         plt.ylim(-100,400)
+        plt.plot([0,100],[50,150],zorder=2)
         plt.show()
 
 if __name__ == "__main__":
-    # # Kevin Durant
-    durant_shots = generate_shots('Kevin','Durant','2017-18')
-    generate_scatter(durant_shots)
-    full_heat_map(durant_shots)
-    success_heat_map(durant_shots)
-
+    # # # Kevin Durant
+    # durant_shots = generate_shots('Kevin','Durant','2017-18')
+    # generate_scatter(durant_shots)
+    # full_heat_map(durant_shots)
+    # success_heat_map(durant_shots)
+    #
     # # James Harden
-    harden_shots = generate_shots('James','Harden','2017-18')
-    generate_scatter(harden_shots)
-    success_heat_map(harden_shots)
+    harden_shots = generate_shots('James','Harden','2017-18') ## consider point weighting
+    # generate_scatter(harden_shots)
+    # success_heat_map(harden_shots)
     full_heat_map(harden_shots)
+    # im = plt.imread('court.png')
+    # implot = plt.imshow(im)
+    # plt.show()
+
+    # print(harden_shots)
